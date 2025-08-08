38bea0cb5272e12d5b39da1bd7f8c6b4c3a6cb04
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-08-08 15:17:04 -0300

    sidebar desktop já criada

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,10 +10,102 @@ import {
 	ShoppingBag,
 	Users,
 } from 'lucide-react';
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '@radix-ui/react-tooltip';
 
 const Sidebar = () => {
 	return (
 		<div className="flex w-full flex-col bg-muted/40">
+			<aside className="fixed inset-y-0 left-0 w-14 z-10 hidden border-r bg-background sm:flex">
+				<nav className="flex flex-col items-center gap-4 px-2 py-5">
+					<TooltipProvider>
+						<Link
+							href="#"
+							className="flex h-9 w-9 shrink-0 items-center justify-center bg-primary text-primary-foreground rounded-full"
+						>
+							<Package className="w-4 h-4" />
+							<span className="sr-only">Dashboard Avatar</span>
+						</Link>
+
+						<Tooltip>
+							<TooltipTrigger asChild>
+								<Link
+									href="#"
+									className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+								>
+									<Home className="w-5 h-5" />
+									<span className="sr-only">Home</span>
+								</Link>
+							</TooltipTrigger>
+							<TooltipContent side="right">Início</TooltipContent>
+						</Tooltip>
+						<Tooltip>
+							<TooltipTrigger asChild>
+								<Link
+									href="#"
+									className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+								>
+									<ShoppingBag className="w-5 h-5" />
+									<span className="sr-only">Pedidos</span>
+								</Link>
+							</TooltipTrigger>
+							<TooltipContent side="right">
+								Pedidos
+							</TooltipContent>
+						</Tooltip>
+						<Tooltip>
+							<TooltipTrigger asChild>
+								<Link
+									href="#"
+									className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+								>
+									<Package className="w-5 h-5" />
+									<span className="sr-only">Produtos</span>
+								</Link>
+							</TooltipTrigger>
+							<TooltipContent side="right">
+								Produtos
+							</TooltipContent>
+						</Tooltip>
+
+						<Tooltip>
+							<TooltipTrigger asChild>
+								<Link
+									href="#"
+									className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+								>
+									<Users className="w-5 h-5" />
+									<span className="sr-only">Clientes</span>
+								</Link>
+							</TooltipTrigger>
+							<TooltipContent side="right">
+								Clientes
+							</TooltipContent>
+						</Tooltip>
+						<Tooltip>
+							<TooltipTrigger asChild>
+								<Link
+									href="#"
+									className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+								>
+									<Settings className="w-5 h-5" />
+									<span className="sr-only">
+										Configurações
+									</span>
+								</Link>
+							</TooltipTrigger>
+							<TooltipContent side="right">
+								Configurações
+							</TooltipContent>
+						</Tooltip>
+					</TooltipProvider>
+				</nav>
+			</aside>
+
 			<div className="sm:hidden flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
 				<header className="sticky top=0 z-30 flex h-14 items-center px-4 border-b bg-background gap-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
 					<Sheet>
