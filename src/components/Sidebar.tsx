69997bc4913027347765f8ca8087c1def365d4cb
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-08-08 14:46:55 -0300

    sidebar mobile já criada

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,27 +2,90 @@ import React from 'react';
 import { Sheet, SheetTrigger, SheetContent } from './ui/sheet';
 import { Button } from './ui/button';
 import Link from 'next/link';
+import {
+	Home,
+	Package,
+	PanelBottom,
+	Settings,
+	ShoppingBag,
+	Users,
+} from 'lucide-react';
 
 const Sidebar = () => {
 	return (
 		<div className="flex w-full flex-col bg-muted/40">
-			<div className="flex flex-col ">
-				<header>
+			<div className="sm:hidden flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+				<header className="sticky top=0 z-30 flex h-14 items-center px-4 border-b bg-background gap-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
 					<Sheet>
 						<SheetTrigger asChild>
-							<Button>
-								<span>Abrir</span>
+							<Button
+								size={'icon'}
+								variant={'outline'}
+								className="sm:hidden"
+							>
+								<PanelBottom className="w-5 h-5" />
+								<span className="sr-only">
+									Abrir ou fechar menu
+								</span>
 							</Button>
 						</SheetTrigger>
 
-						<SheetContent>
-							<nav>
-								<Link href="#">
-									<span>Logo</span>
+						<SheetContent side="left" className="sm:max-w-xs p-5">
+							<nav className="grid gap-6 text-lg font-medium">
+								<Link
+									href="#"
+									className="flex h-10 w-10 bg-primary rounded-full text-lg items-center justify-center text-primary-foreground md:text-base gap-2"
+									prefetch={false}
+								>
+									<Package className="w-5 h-5 transition-all" />
+									<span className="sr-only">Logo</span>
+								</Link>
+
+								<Link
+									href="#"
+									className="flex items-center gap-3 px-2.5 text-muted-foreground hover:text-foreground"
+									prefetch={false}
+								>
+									<Home className="w-5 h-5 transition-all" />
+									<span>Início</span>
+								</Link>
+								<Link
+									href="#"
+									className="flex items-center gap-3 px-2.5 text-muted-foreground hover:text-foreground"
+									prefetch={false}
+								>
+									<ShoppingBag className="w-5 h-5 transition-all" />
+									<span>Pedidos</span>
+								</Link>
+								<Link
+									href="#"
+									className="flex items-center gap-3 px-2.5 text-muted-foreground hover:text-foreground"
+									prefetch={false}
+								>
+									<Package className="w-5 h-5 transition-all" />
+									<span>Produtos</span>
+								</Link>
+								<Link
+									href="#"
+									className="flex items-center gap-3 px-2.5 text-muted-foreground hover:text-foreground"
+									prefetch={false}
+								>
+									<Users className="w-5 h-5 transition-all" />
+									<span>Clientes</span>
+								</Link>
+								<Link
+									href="#"
+									className="flex items-center gap-3 px-2.5 text-muted-foreground hover:text-foreground"
+									prefetch={false}
+								>
+									<Settings className="w-5 h-5 transition-all" />
+									<span>Configurações</span>
 								</Link>
 							</nav>
 						</SheetContent>
 					</Sheet>
+
+					<h2>Menu</h2>
 				</header>
 			</div>
 		</div>
